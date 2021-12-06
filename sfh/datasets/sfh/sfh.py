4895e0207b566006672efddb9c854c08eedf1754
Lauri Laatu <lauri.laatu@aalto.fi>
2021-12-06 22:43:36 +0100

    Read csv files to dicts of tensors

--- sfh/datasets/sfh/sfh.py
+++ sfh/datasets/sfh/sfh.py
@@ -1,7 +1,7 @@
 """sfh dataset."""
 
 import tensorflow_datasets as tfds
-
+import pandas as pd
 # TODO(sfh): Markdown description  that will appear on the catalog page.
 _DESCRIPTION = """
 Description is **formatted** as markdown.
@@ -14,6 +14,7 @@ It should also contain any processing which has been applied (if any),
 _CITATION = """
 """
 
+N_TIMESTEPS = 100
 
 class Sfh(tfds.core.GeneratorBasedBuilder):
     """DatasetBuilder for sfh dataset."""
@@ -32,33 +33,53 @@ class Sfh(tfds.core.GeneratorBasedBuilder):
             features=tfds.features.FeaturesDict(
                 {
                     # These are the features of your dataset like images, labels ...
-                    "image": tfds.features.Image(shape=(None, None, 3)),
-                    "label": tfds.features.ClassLabel(names=["no", "yes"]),
+                    "time": tfds.features.Tensor(shape=(N_TIMESTEPS, 1)),
+                    "SFR_halfRad": tfds.features.Tensor(shape=(N_TIMESTEPS, 1)),
+                    "SFR_Rad": tfds.features.Tensor(shape=(N_TIMESTEPS, 1)),
+                    "SFR_Max": tfds.features.Tensor(shape=(N_TIMESTEPS, 1)),
+                    "Mstar_Half": tfds.features.Tensor(shape=(N_TIMESTEPS, 1)),
+                    "Mstar": tfds.features.Tensor(shape=(N_TIMESTEPS, 1)),
+
+
+                    #"label": tfds.features.ClassLabel(names=["no", "yes"]),
                 }
             ),
             # If there's a common (input, target) tuple from the
             # features, specify them here. They'll be used if
             # `as_supervised=True` in `builder.as_dataset`.
-            supervised_keys=("image", "label"),  # Set to `None` to disable
+            supervised_keys=(None),  # Set to `None` to disable
             homepage="https://dataset-homepage/",
             citation=_CITATION,
         )
 
+    def csv_to_np(self, path):
+
+        keys = ["time", "SFR_halfRad", "SFR_Rad", "SFR_Max", "Mstar_Half", "Mstar"]
+ 
+        data = {}
+        df = pd.read_csv(path)
+        timesteps = df[['SnapNum']].values
+        
+        for k in keys:
+            d = np.zeros((N_TIMESTEPS, 1))
+            d[timesteps, 0] = df[[k]].values
+            data[k] = d
+
+        return data
+
+
     def _split_generators(self, dl_manager: tfds.download.DownloadManager):
         """Returns SplitGenerators."""
         # TODO(sfh): Downloads the data and defines the splits
-        path = dl_manager.download_and_extract("https://todo-data-url")
+        path = dl_manager.extract(os.path.join(dl_manager.manual_dir, "cats_SFH"))#download_and_extract("https://todo-data-url")
 
         # TODO(sfh): Returns the Dict[split names, Iterator[Key, Example]]
         return {
-            "train": self._generate_examples(path / "train_imgs"),
+            "train": self._generate_examples(path),
         }
 
     def _generate_examples(self, path):
         """Yields examples."""
         # TODO(sfh): Yields (key, example) tuples from the dataset
-        for f in path.glob("*.jpeg"):
-            yield "key", {
-                "image": f,
-                "label": "yes",
-            }
+        for f in path.glob("*.csv"):
+            yield "key", csv_to_np(path)
