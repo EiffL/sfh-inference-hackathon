40934de7337b48aca6840d0e87e25b51c897dfd5
Hubert Bretonniere <hubert.bretonniere@ias.u-psud.fr>
2021-12-06 18:34:14 +0100

    utils for finding the time of the half mass

---
+++ utils.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+def find_halftime(gal_sfh):
+
+  ''' compute the half mass and the half time of a galaxy 
+        Input: the sfh
+        Output: the half mass, the corresponding half time, and the number of degenerecies
+  '''
+  half_mass = min(gal_sfh.Mstar_Half, key=lambda x:abs(x-np.max(gal_sfh.Mstar_Half)/2.)) # find mass closest to the half mass
+  half_mass_indices = np.where(gal_sfh.Mstar_Half == half_mass)[0]  # find the corresponding indices
+  nb_soluce = len(half_mass_indices) # find the number of indices
+  half_mass_index = half_mass_indices[0]  # chose the first index for the half mass
+  half_time = gal_sfh.time[half_mass_index]  # find the corresponding half time
+
+  return half_mass, half_time, len(nb_soluce)
