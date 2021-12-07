68825b4bf2ce0817e01dc3eaef48d99789343185
Benjamin Desrousseaux <benjamin.desrousseaux@univ-rennes1.fr>
2021-12-07 15:10:06 +0100

    Add debug infos

--- sfh/datasets/mergers/kinetic.py
+++ sfh/datasets/mergers/kinetic.py
@@ -77,17 +77,18 @@ class MergersKinetic(tfds.core.GeneratorBasedBuilder):
     snaps_dir = os.path.join(os.path.dirname(__file__), './')
     snaps = pd.read_csv(snaps_dir+"/snaps.csv", index_col=0, names=['sn', 'z', 'age', 'lbt'])
     for i, fits_file in enumerate(os.listdir(fits_dir_path)):
-      # Get object_id from the current FITS file name
-      object_id = int(fits_file.split('_')[3])
-      # Extract image data from the FITS file
-      image = fits.getdata(fits_dir_path+fits_file, ext=0)
-      # Get snapshot number of the last major merger for the current object_id from the mergers_data dataframe
-      SnapNumLastMajorMerger = mergers_data.loc[object_id,'SnapNumLastMajorMerger']
       try:
+        # Get object_id from the current FITS file name
+        object_id = int(fits_file.split('_')[3])
+        # Extract image data from the FITS file
+        image = fits.getdata(fits_dir_path+fits_file, ext=0)
+        # Get snapshot number of the last major merger for the current object_id from the mergers_data dataframe
+        SnapNumLastMajorMerger = mergers_data.loc[object_id,'SnapNumLastMajorMerger']
         # Convert snapshot number to lookback time using the snaps dataframe
         last_major_merger = snaps.loc[SnapNumLastMajorMerger,'lbt']
         # Yiel with i because in our case object_id will be the same for the 4 different projections
         yield i, {'image': image.astype("float32"), 'last_major_merger': last_major_merger, 'object_id': object_id}
       except:
-        print("File ", fits_file, " not added to the dataset (no Major Merger event -> SnapNumLastMajorMerger=-1)")
+        print("File ", fits_file, " not added to the dataset: object_id=", object_id, 
+        " SnapNumLastMajorMerger=",SnapNumLastMajorMerger, " last_major_merger: ", last_major_merger)
 
