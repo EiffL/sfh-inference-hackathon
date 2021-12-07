1713d8f3529075fbda758720cf7992838eac077d
Cyrille Rosset <rosset@in2p3.fr>
2021-12-07 21:53:00 +0100

    reorganize directories

---
+++ sfh-sed/python/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='sfhsed',
+      description='Tools for SFH from SED project - AstroInfo 2021',
+      author='Hubert, Adnan, Cyrille',
+      packages=find_packages(),
+      install_requires=[])
