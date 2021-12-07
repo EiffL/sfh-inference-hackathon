90292030d3dc690bb1b79c0c958c60877f09d36b
Cyrille Rosset <rosset@in2p3.fr>
2021-12-07 19:02:00 +0100

    fix fluxes and magnitudes confusion

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import statsmodels.api as sm
+#import statsmodels.api as sm
 import numpy as np
 
 class SubHalos:
@@ -24,7 +24,7 @@ class SubHalos:
 class SubHalo:
     def __init__(self, row, wl=None, times=None):
         self._shid = row[0]
-        self._fluxes = row[1:144]
+        self._mags = row[1:144]
         self._mstar = row[244:344]
         self._sfr = row[144:244]
         self._wl = wl
@@ -35,8 +35,16 @@ class SubHalo:
         return self._shid
 
     @property
+    def mags(self):
+        return self._mags
+
+    @property
     def fluxes(self):
-        return self._fluxes
+        # compute apparent magnitude, assuming 20pc
+        app_mags = self.mags + 5.0 * (np.log10(20e6) - 1.0)
+        # convert to Jy
+        fluxes = 10**(0.4 * (-app_mags + 8.90)) 
+        return fluxes
 
     @property
     def mstar(self):
@@ -130,59 +138,59 @@ _CITATION = """
 
 
 class MyDataset(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for my_dataset dataset."""
+    """DatasetBuilder for my_dataset dataset."""
 
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
+    VERSION = tfds.core.Version('1.0.0')
+    RELEASE_NOTES = {
       '1.0.0': 'Initial release.',
-  }
+    }
 
-  def _info(self) -> tfds.core.DatasetInfo:
-    """Returns the dataset metadata."""
-    # TODO(my_dataset): Specifies the tfds.core.DatasetInfo object
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-          'flux':tfds.features.Tensor(shape=(1, 143 ), dtype=tf.float32),
-          'quantile':tfds.features.Scalar(shape=(1), dtype=tf.float32),
-            # These are the features of your dataset like images, labels ...
-        }),
-        # If there's a common (input, target) tuple from the
-        # features, specify them here. They'll be used if
-        # `as_supervised=True` in `builder.as_dataset`.
-        supervised_keys=None,  # e.g. ('image', 'label')
-        #homepage='https://dataset-homepage/',
-        citation=_CITATION,
-    )
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Returns the dataset metadata."""
+        # TODO(my_dataset): Specifies the tfds.core.DatasetInfo object
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=tfds.features.FeaturesDict({
+              'flux':tfds.features.Tensor(shape=(1, 143 ), dtype=tf.float32),
+              'quantile':tfds.features.Scalar(shape=(1), dtype=tf.float32),
+                # These are the features of your dataset like images, labels ...
+            }),
+            # If there's a common (input, target) tuple from the
+            # features, specify them here. They'll be used if
+            # `as_supervised=True` in `builder.as_dataset`.
+            supervised_keys=None,  # e.g. ('image', 'label')
+            #homepage='https://dataset-homepage/',
+            citation=_CITATION,
+        )
 
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
-    # TODO(my_dataset): Downloads the data and defines the splits
-    # dl_manager is a tfds.download.DownloadManager that can be used to
-    # download and extract URLs
-    catalog_path = 'path'
-    return [
-        tfds.core.SplitGenerator(
-            name=tfds.Split.TRAIN,
-            # These kwargs will be passed to _generate_examples
-            gen_kwargs={'catalog_path': catalog_path},
-        ),
-    ]
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Returns SplitGenerators."""
+        # TODO(my_dataset): Downloads the data and defines the splits
+        # dl_manager is a tfds.download.DownloadManager that can be used to
+        # download and extract URLs
+        catalog_path = 'path'
+        return [
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TRAIN,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={'catalog_path': catalog_path},
+            ),
+        ]
 
-def _generate_examples(self, fits_dir_path):
-    """Yields examples."""
-    from astropy.io import fits # To open FITS files
-    from astropy.cosmology import Planck13 # To convert redshift to loopback time
-    import pandas as pd # To extract the SnapNumLastMajorMerger values from TNG100_SDSS_MajorMergers.csv
+    def _generate_examples(self, fits_dir_path):
+        """Yields examples."""
+        from astropy.io import fits # To open FITS files
+        from astropy.cosmology import Planck13 # To convert redshift to loopback time
+        import pandas as pd # To extract the SnapNumLastMajorMerger values from TNG100_SDSS_MajorMergers.csv
 
-    # Create new dataframe with the columns 'Illustris_ID' and 'SnapNumLastMajorMerger'
-    # of the TNG100_SDSS_MajorMergers.csv file
-    flux_mass_data =  Table.read(fits_dir_path)
-    # recover fluxes
-    for t in flux_mass_data:
-      flux = t[1:144]
-      quantile = t['quantile']
-      object_id = t['halo_id']
-    # Yiel with i because in our case object_id will be the same for the 4 different projections
-        yield t, {'flux': flux.astype("float32"), 'quantile': quantile.astype('float32'), 'object_id': object_id}
+        # Create new dataframe with the columns 'Illustris_ID' and 'SnapNumLastMajorMerger'
+        # of the TNG100_SDSS_MajorMergers.csv file
+        flux_mass_data =  Table.read(fits_dir_path)
+        # recover fluxes
+        for t in flux_mass_data:
+            flux = t[1:144]
+            quantile = t['quantile']
+            object_id = t['halo_id']
+            # Yiel with i because in our case object_id will be the same for the 4 different projections
+            yield t, {'flux': flux.astype("float32"), 'quantile': quantile.astype('float32'), 'object_id': object_id}
