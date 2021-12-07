527fbc1e3cd8fa82981f77a4dafaa1246867cd99
Ghribi Adnan <ghribi@ganm1874.local>
2021-12-07 16:11:12 +0100

    added data_set.py

--- utils.py
+++ utils.py
@@ -59,3 +59,77 @@ def smoothen(x, y):
     # smoothen data with frac = 0.2 to adjuct if needed
     lowess = sm.nonparametric.lowess(y, x, frac=0.2)
     return lowess
+
+
+# here begins my_dataset.py
+"""my_dataset dataset."""
+import numpy as np
+from astropy.table import Table
+import tensorflow_datasets as tfds
+
+# TODO(my_dataset): Markdown description  that will appear on the catalog page.
+_DESCRIPTION = """
+"""
+
+# TODO(my_dataset): BibTeX citation
+_CITATION = """
+"""
+
+
+class MyDataset(tfds.core.GeneratorBasedBuilder):
+  """DatasetBuilder for my_dataset dataset."""
+
+  VERSION = tfds.core.Version('1.0.0')
+  RELEASE_NOTES = {
+      '1.0.0': 'Initial release.',
+  }
+
+  def _info(self) -> tfds.core.DatasetInfo:
+    """Returns the dataset metadata."""
+    # TODO(my_dataset): Specifies the tfds.core.DatasetInfo object
+    return tfds.core.DatasetInfo(
+        builder=self,
+        description=_DESCRIPTION,
+        features=tfds.features.FeaturesDict({
+          'flux':tfds.features.Tensor(shape=(1, 143 ), dtype=tf.float32),
+          'quantile':tfds.features.Scalar(shape=(1), dtype=tf.float32),
+            # These are the features of your dataset like images, labels ...
+        }),
+        # If there's a common (input, target) tuple from the
+        # features, specify them here. They'll be used if
+        # `as_supervised=True` in `builder.as_dataset`.
+        supervised_keys=None,  # e.g. ('image', 'label')
+        #homepage='https://dataset-homepage/',
+        citation=_CITATION,
+    )
+
+  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+    """Returns SplitGenerators."""
+    # TODO(my_dataset): Downloads the data and defines the splits
+    # dl_manager is a tfds.download.DownloadManager that can be used to
+    # download and extract URLs
+    catalog_path = 'path'
+    return [
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            # These kwargs will be passed to _generate_examples
+            gen_kwargs={'catalog_path': catalog_path},
+        ),
+    ]
+
+def _generate_examples(self, fits_dir_path):
+    """Yields examples."""
+    from astropy.io import fits # To open FITS files
+    from astropy.cosmology import Planck13 # To convert redshift to loopback time
+    import pandas as pd # To extract the SnapNumLastMajorMerger values from TNG100_SDSS_MajorMergers.csv
+
+    # Create new dataframe with the columns 'Illustris_ID' and 'SnapNumLastMajorMerger'
+    # of the TNG100_SDSS_MajorMergers.csv file
+    flux_mass_data =  Table.read(fits_dir_path)
+    # recover fluxes
+    for t in flux_mass_data:
+      flux = t[1:144]
+      quantile = t['quantile']
+      object_id = t['halo_id']
+    # Yiel with i because in our case object_id will be the same for the 4 different projections
+        yield t, {'flux': flux.astype("float32"), 'quantile': quantile.astype('float32'), 'object_id': object_id}
