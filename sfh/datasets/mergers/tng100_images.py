ca052ab9ac3a99246fc0bd3bda5b8b278037d1c8
Elisabeth Sola <elisabeth.sola@outlook.fr>
2021-12-07 17:39:13 +0100

    Modify paths for Jean Zay

--- sfh/datasets/mergers/tng100_images.py
+++ sfh/datasets/mergers/tng100_images.py
@@ -87,10 +87,11 @@ class Tng100Images(tfds.core.GeneratorBasedBuilder):
     # TODO(tng100_images): Downloads the data and defines the splits
     # dl_manager is a tfds.download.DownloadManager that can be used to
     # download and extract URLs
-    manual_dir='/content'
-    cat_snapshot_path=manual_dir+"/drive/MyDrive/Colab Notebooks/corresp_snapshot_lookback.csv"
-    cat_merger_path=manual_dir+"/data/mergers/TNG100_SDSS_MajorMergers.csv"
-    img_dir=manual_dir+"/data/images/TNG100/sdss/sn99/Outputs/"
+    data_path = os.path.expandvars("$ALL_CCFRWORK/SFH/tng100/")
+    snaps_dir = os.path.join(os.path.dirname(__file__), './')
+    cat_snapshot_path=snaps_dir+"/snap2lookback.csv"
+    cat_merger_path=data_path+"/mergers/TNG100_SDSS_MajorMergers.csv"
+    img_dir=data_path+"/images/TNG100/sdss/sn99/Outputs/"
     #For local test
     #data_path = os.path.join(dl_manager.manual_dir, img_dir)
     #For Jean Zay
@@ -106,11 +107,11 @@ class Tng100Images(tfds.core.GeneratorBasedBuilder):
   def _generate_examples(self,img_dir=None,cat_snapshot_path=None,cat_merger_path=None):
     """Yields examples."""
     # TODO(tng100_images): Yields (key, example) tuples from the dataset
-    catalog_merger_time=pd.read_csv(cat_merger_path)  
+    catalog_merger_time=pd.read_csv(cat_merger_path)
     catalog_snapshot=pd.read_csv(cat_snapshot_path)
     # Keep the IDs of the galaxy for which the four bands are available
     gal_ids=keep_common_filters(img_dir)
- 
+
     # For each galaxy, stacks the four bands
     for i in range(len(gal_ids)):
       try:
@@ -119,8 +120,8 @@ class Tng100Images(tfds.core.GeneratorBasedBuilder):
         #Retrieves the lookback time of the last major merger
         num_last_merger=int(catalog_merger_time[catalog_merger_time["Illustris_ID"]==gal_ids[i]]["SnapNumLastMajorMerger"])
         lbt=float(catalog_snapshot[catalog_snapshot["Snapshot"]==num_last_merger]["Lookback"])
-      #Returns the image, the galaxy ID and the lookback time of the last major merger 
-      except: 
+      #Returns the image, the galaxy ID and the lookback time of the last major merger
+      except:
         print("Problem for gal_ids",gal_ids[i])
         continue
 
@@ -128,6 +129,3 @@ class Tng100Images(tfds.core.GeneratorBasedBuilder):
                   "last_major_merger":lbt,
                 "object_id":gal_ids[i]
       }
-    
-    
-
