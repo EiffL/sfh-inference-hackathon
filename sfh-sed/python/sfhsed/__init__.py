cdeab041bf27d814a2cd52e03d4eb3b641a223ef
Cyrille Rosset <rosset@in2p3.fr>
2021-12-08 10:29:45 +0100

    fix rebase bugs

--- sfh-sed/python/sfhsed/__init__.py
+++ sfh-sed/python/sfhsed/__init__.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-#from sfhsed import utils
-=======
 from sfhsed import utils
->>>>>>> reorganize directories
-=======
-#from sfhsed import utils
->>>>>>> move notebook file
