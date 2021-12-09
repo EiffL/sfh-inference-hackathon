904b5b52b0529145b25fad174faa15c998bc9564
Nicolas Cerardi <nicolas.cerardi@gmail.com>
2021-12-09 17:52:32 +0100

    SFR0 and time conditional

--- sfh/model_utils.py
+++ sfh/model_utils.py
@@ -8,22 +8,29 @@ def preprocessing(example):
 
 def preprocessing_wmass(example):
     mass = example['Mstar'][:,0]
-    mass_half = example['Mstar_Half'][:,0]
+    sfr_z0 = example['SFR_Max'][:,0]
+    time = example['time'][:,0]
+    
     tiler = tf.constant([100])
     mass = tf.reshape(tf.tile(mass, tiler),(-1,100,1))
-    mass_half = tf.reshape(tf.tile(mass_half, tiler),(-1,100,1))
+    sfr_z0 = tf.reshape(tf.tile(sfr_z0, tiler),(-1,100,1))
+    time = tf.reshape(tf.tile(time, tiler),(-1,100,1))
+    
     sfr = tf.math.add(tf.reshape(example['SFR_Max'],(-1,100,1)), 1e-5)
-    res = tf.concat([sfr, mass, mass_half], axis=2)
+    res = tf.concat([sfr, mass, sfr_z0, time], axis=2)
     return res, res
 
 def preprocessing_wmass_atan(example):
     mass = example['Mstar'][:,0]
-    mass_half = example['Mstar_Half'][:,0]
+    sfr_z0 = example['SFR_Max'][:,0]
+    time = example['time'][:,0]
     tiler = tf.constant([100])
     mass = tf.reshape(tf.tile(mass, tiler),(-1,100,1))
-    mass_half = tf.reshape(tf.tile(mass_half, tiler),(-1,100,1))
+    sfr_z0 = tf.reshape(tf.tile(sfr_z0, tiler),(-1,100,1))
+    time = tf.reshape(tf.tile(time, tiler),(-1,100,1))
+    
     sfr = tf.scalar_mul(2/np.pi, tf.math.atan(tf.math.add(tf.reshape(example['SFR_Max'],(-1,100,1)), 1e-5)))
-    res = tf.concat([sfr, mass, mass_half], axis=2)
+    res = tf.concat([sfr, mass, sfr_z0, time], axis=2)
     return res, res
 
 def input_fn(mode='train', batch_size=64, 
@@ -49,15 +56,18 @@ def input_fn(mode='train', batch_size=64,
     dataset = dataset.prefetch(-1)       # fetch next batches while training current one (-1 for autotune)
     return dataset
 
-def predictor(model, sample_size, nsteps=100, n_channels=1, mode='sample', arctan=False):
+def predictor(model, sample_size, nsteps=100, n_channels=1, mode='sample', 
+              arctan=False, M0=None, SFR0=None):
     """
     mode should be either 'sample' or 'mean'
     """
     assert mode in ['sample', 'mean']
     res = np.zeros((sample_size, nsteps, n_channels))
-    if n_channels==3:
-        res[:,:,1] = np.random.uniform(10**(8.5-10), 1, size=(sample_size,))
-        res[:,:,2] = res[:,:,1]*np.random.uniform(0.6, 0.7, size=(sample_size,))
+    if n_channels>=3:
+        if M0 is not None: res[:,:,1] = M0
+        else : res[:,:,1] = 10**(np.random.uniform(8.5, 10, size=(1,))-10)
+        if SFR0 is not None: res[:,:,2] = SFR0
+        else: res[:,:,2] = res[:,:,1]*np.random.uniform(0.0001, 1, size=(1,))
     for i in range(nsteps):
         if mode=='sample':
             tmp = model(res).sample()
@@ -67,13 +77,13 @@ def predictor(model, sample_size, nsteps=100, n_channels=1, mode='sample', arcta
     if not arctan :
         return res[:,:,0]
     else :
-        return np.tan(res[:,:,0])*np.pi/2
+        return np.tan(res[:,:,0])*np.pi/2, res[:,0,1], res[:,0,2]
 
 def pass_sample(model, sample):
     """
     For now only works with 1 sample, not batch !
     """
-    sample = tf.reshape(sample,(1, 100, 1))
+    sample = tf.reshape(sample,(1, 100, 4))
     mean = model(sample).mean()
     std = model(sample).stddev()
     p_sample = model(sample).sample()
@@ -88,9 +98,9 @@ def finish_sample(model, sample, nsteps=100, mode='sample'):
     sample : a 1d Tensor SFR sequence, of cardinality<nsteps
     nsteps : total length of output sequence
     """
-    assert sample.numpy().reshape((-1,)).shape[0]<nsteps
-    n_remain = nsteps - sample.numpy().reshape((-1,)).shape[0]
-    res = np.zeros((1, nsteps,1))
+    assert sample.numpy().reshape((-1,4)).shape[0]<nsteps
+    n_remain = nsteps - sample.numpy().reshape((-1,4)).shape[0]
+    res = np.zeros((1, nsteps,4))
     res[0,:nsteps-n_remain,0] = sample.numpy()
     for i in range(nsteps-n_remain, nsteps):
         if mode=='sample':
