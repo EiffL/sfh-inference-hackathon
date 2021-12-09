b3ba0a66ae2587eb506d845c8db6fade5f4a6b2f
Benjamin Desrousseaux <benjamin.desrousseaux@univ-rennes1.fr>
2021-12-09 11:04:10 +0100

    Update model to new img size

--- scripts/mergers_kinetic/model.py
+++ scripts/mergers_kinetic/model.py
@@ -2,7 +2,7 @@ import tensorflow.keras as tfk
 
 def create_model():
   model = tfk.models.Sequential()
-  model.add(tfk.layers.Conv2D(32, kernel_size=5, padding='same', input_shape=(3,128,128), activation='elu', strides=2))
+  model.add(tfk.layers.Conv2D(32, kernel_size=5, padding='same', input_shape=(128,128,2), activation='elu', strides=2))
   model.add(tfk.layers.BatchNormalization())
   
   #model.add(tfk.layers.Conv2D(64, kernel_size=3, padding='same', activation='elu'))
