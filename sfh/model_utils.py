2826a4076a429e3fce9b7e680861a7718a0e6e70
Nicolas Cerardi <nicolas.cerardi@gmail.com>
2021-12-09 22:00:55 +0100

    fix tiling

--- sfh/model_utils.py
+++ sfh/model_utils.py
@@ -7,29 +7,33 @@ def preprocessing(example):
            tf.reshape(example['SFR_Max'],(-1,100,1))
 
 def preprocessing_wmass(example):
-    mass = example['Mstar'][:,0]
-    sfr_z0 = example['SFR_Max'][:,0]
-    time = example['time'][:,0]
+    sfr = tf.math.add(tf.reshape(example['SFR_Max'],(-1,100,1)), 1e-5)
+    mass = tf.reshape(tf.math.log(example['Mstar'][:,0]), (-1,1))
+    sfr_z0 = tf.reshape(sfr[:,0,0], (-1,1))
+    time = example['time']
     
-    tiler = tf.constant([100])
+    tiler = tf.constant([1, 100])
+    
     mass = tf.reshape(tf.tile(mass, tiler),(-1,100,1))
     sfr_z0 = tf.reshape(tf.tile(sfr_z0, tiler),(-1,100,1))
-    time = tf.reshape(tf.tile(time, tiler),(-1,100,1))
+    time = tf.reshape(time,(-1,100,1))
     
-    sfr = tf.math.add(tf.reshape(example['SFR_Max'],(-1,100,1)), 1e-5)
     res = tf.concat([sfr, mass, sfr_z0, time], axis=2)
     return res, res
 
 def preprocessing_wmass_atan(example):
-    mass = example['Mstar'][:,0]
-    sfr_z0 = example['SFR_Max'][:,0]
-    time = example['time'][:,0]
-    tiler = tf.constant([100])
+    sfr = tf.scalar_mul(2/np.pi, tf.math.atan(tf.math.add(tf.reshape(example['SFR_Max'],(-1,100,1)), 1e-5)))
+    mass = tf.reshape(tf.math.log(example['Mstar'][:,0]), (-1,1))
+    #print(sfr[:,0].get_shape, example['Mstar'][:,0].get_shape())
+
+    sfr_z0 = tf.reshape(sfr[:,0,0], (-1,1))
+    time = example['time']
+    tiler = tf.constant([1, 100])
+    
     mass = tf.reshape(tf.tile(mass, tiler),(-1,100,1))
     sfr_z0 = tf.reshape(tf.tile(sfr_z0, tiler),(-1,100,1))
-    time = tf.reshape(tf.tile(time, tiler),(-1,100,1))
+    time = tf.reshape(time,(-1,100,1))
     
-    sfr = tf.scalar_mul(2/np.pi, tf.math.atan(tf.math.add(tf.reshape(example['SFR_Max'],(-1,100,1)), 1e-5)))
     res = tf.concat([sfr, mass, sfr_z0, time], axis=2)
     return res, res
 
@@ -57,7 +61,7 @@ def input_fn(mode='train', batch_size=64,
     return dataset
 
 def predictor(model, sample_size, nsteps=100, n_channels=1, mode='sample', 
-              arctan=False, M0=None, SFR0=None):
+              arctan=False, M0=None, SFR0=None, time=None):
     """
     mode should be either 'sample' or 'mean'
     """
@@ -67,7 +71,9 @@ def predictor(model, sample_size, nsteps=100, n_channels=1, mode='sample',
         if M0 is not None: res[:,:,1] = M0
         else : res[:,:,1] = 10**(np.random.uniform(8.5, 10, size=(1,))-10)
         if SFR0 is not None: res[:,:,2] = SFR0
-        else: res[:,:,2] = res[:,:,1]*np.random.uniform(0.0001, 1, size=(1,))
+        else: res[:,:,2] = np.random.uniform(0.0001, 1, size=(1,))
+    if n_channels==4:
+        res[:,:,-1] = time
     for i in range(nsteps):
         if mode=='sample':
             tmp = model(res).sample()
@@ -77,7 +83,7 @@ def predictor(model, sample_size, nsteps=100, n_channels=1, mode='sample',
     if not arctan :
         return res[:,:,0]
     else :
-        return np.tan(res[:,:,0])*np.pi/2, res[:,0,1], res[:,0,2]
+        return np.tan(res[:,:,0]*np.pi/2), res[:,0,1], res[:,0,2]
 
 def pass_sample(model, sample):
     """
