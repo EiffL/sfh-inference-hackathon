5d1353a70085dbd9e51fdafdb8369807e07076c3
Benjamin Desrousseaux <benjamin.desrousseaux@univ-rennes1.fr>
2021-12-09 22:00:37 +0100

    Simplify gmm model

--- scripts/mergers_kinetic/model_gmm.py
+++ scripts/mergers_kinetic/model_gmm.py
@@ -8,7 +8,7 @@ tfd = tfp.distributions
 def create_model():
 
     # Number of components in the Gaussian Mixture
-    num_components = 3
+    num_components = 4
     # Shape of the distribution
     event_shape = [1]
     # Utility function to compute how many parameters this distribution requires
@@ -20,17 +20,17 @@ def create_model():
     #Define the model
     model = tfk.models.Sequential()
     #1st layer
-    model.add(tfk.layers.Conv2D(32, kernel_size=3, padding='same',input_shape=(128,128,2), activation='elu', strides=2))
+    model.add(tfk.layers.Conv2D(32, kernel_size=3, padding='same',input_shape=(128,128,2), activation='relu', strides=2))
     model.add(tfk.layers.BatchNormalization())
     #2nd layer
-    model.add(tfk.layers.Conv2D(64, kernel_size=3, padding='same', activation='relu'))
-    model.add(tfk.layers.BatchNormalization())
+    #model.add(tfk.layers.Conv2D(64, kernel_size=3, padding='same', activation='relu'))
+    #model.add(tfk.layers.BatchNormalization())
     #1st layer
-    model.add(tfk.layers.Conv2D(128, kernel_size=3, padding='same', activation='relu'))
-    model.add(tfk.layers.BatchNormalization())
+    #model.add(tfk.layers.Conv2D(128, kernel_size=3, padding='same', activation='relu'))
+    #model.add(tfk.layers.BatchNormalization())
     #2nd layer
-    model.add(tfk.layers.Conv2D(256, kernel_size=3, padding='same', activation='relu'))
-    model.add(tfk.layers.BatchNormalization())
+    #model.add(tfk.layers.Conv2D(256, kernel_size=3, padding='same', activation='relu'))
+    #model.add(tfk.layers.BatchNormalization())
     #Flatten layer
     model.add(tfk.layers.Flatten())
     #Dense
@@ -38,8 +38,8 @@ def create_model():
     model.add(tfk.layers.Dense(256,activation='relu'))
     model.add(tfk.layers.Dropout(0.5))
 
-    model.add(tfk.layers.Dense(128,activation='tanh'))
-    model.add(tfk.layers.Dropout(0.5))
+    model.add(tfk.layers.Dense(128,activation='relu'))
+    model.add(tfk.layers.Dropout(0.2))
 
     #Predict a distribution
     model.add(tfkl.Dense(params_size))
